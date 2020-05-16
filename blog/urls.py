0b0d02f16ec7cdd0a5523523c85e6206fe3668ae
syota-sidara <ss045.official@gmail.com>
2020-05-16 11:48:27 +0900

    complete

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import PostListView
+from . import views
 
 urlpatterns = [
-  path('', PostListView.as_view(), name='blog-home'),
+    path('', views.PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create')
 ]
+
+# <pk> => primary keyの略。番号を降ってくれる
